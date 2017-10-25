79cd3694cb56b69afa67ed13108364605a9df785
Noel Deguzman <noel.deguzman@me.com>
2017-10-24 23:36:28 -0700

    Add PRY and run Pry Debug Session

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,39 +1,46 @@
 require_relative '../models/book'
+require "pry"
 
 class BooksController < BlocWorks::Controller
   attr_accessor :book, :books 
 
   def welcome
+    binding.pry   
     @book = Book.first
     render :welcome
   end
 
   def index
+    binding.pry   
     @books = Book.all
     render :index
   end
 
   def new
+    binding.pry   
     render :new 
   end
 
   def show
+    binding.pry   
     id = @env["QUERY_STRING"].split('=')[1].to_i
     @book = Book.find(id)
     render :show
   end
 
   def edit
+    binding.pry   
     id = @env["QUERY_STRING"].split('=')[1].to_i
     @book = Book.find(id)
     render :edit
   end
 
   def create
+    binding.pry   
     request = Rack::Request.new(@env)
-    title = request.params["title"]
+    title   = request.params["title"]
     Book.new(title).save
-    @book = Book.last
+    @book   = Book.last
     if @book.title == title
       redirect_to '/books/show'
     else
@@ -43,8 +50,9 @@ class BooksController < BlocWorks::Controller
   end
 
   def update
+    binding.pry   
     request = Rack::Request.new(@env)
-    id = request.params["id"].to_i
+    id      = request.params["id"].to_i
 
     puts request.params
 
@@ -58,8 +66,9 @@ class BooksController < BlocWorks::Controller
   end
 
   def destroy
+    binding.pry   
     request = Rack::Request.new(@env)
-    id = request.params["id"].to_i
+    id      = request.params["id"].to_i
 
     Book.destroy(id)
 
--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require "bloc_record"
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "models")
 
-BlocRecord.connect_to("db/db.sqlite", "sqlite3")
+BlocRecord.connect_to("db/db.sqlite")
 
 module BlocBooks
   class Application < BlocWorks::Application
